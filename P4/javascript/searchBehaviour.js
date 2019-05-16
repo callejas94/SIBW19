77aa7ae685010746a05b663ccf811a0f66d6f2a9
Guillermo Bueno Vargas <Guillergood@gmail.com>
2019-05-16 17:16:07 +0200

    Actualizado la busqueda
    Busqueda final de eventos

---
+++ P4/javascript/searchBehaviour.js
@@ -0,0 +1,66 @@
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    continua = true;
+    const $filtradoEventos = document.querySelector('#filtradoEventos');
+
+
+	const typeHandlerAdvertencias = function(e) {
+	  BusquedaEventos();
+	}
+
+	$filtradoEventos.addEventListener('input', typeHandlerAdvertencias) // register for oninput
+	$filtradoEventos.addEventListener('propertychange', typeHandlerAdvertencias) // for IE8
+});
+
+
+function BusquedaEventos() {
+    var inputTexto = document.getElementById('filtradoEventos').value;
+    var lista = document.getElementById('contenedores');
+
+    if(inputTexto.length > 0){
+    	FiltraPorTexto(inputTexto,lista);
+    }
+    else{
+    	MuestraTodosEventos(lista);
+    }
+}
+
+function MuestraTodosEventos(lista){
+	var items = lista.getElementsByTagName("a");
+	for (var i = 0; i < items.length; ++i) {
+	  items[i].style.display = "inline-block";
+	}
+}
+
+function FiltraPorTexto(inputTexto, lista){
+
+	var listaFiltrada = [];
+	var listaNormal = [];
+
+	var items = lista.getElementsByTagName("a");
+	for (var i = 0; i < items.length; ++i) {
+	  // do something with items[i], which is a <li> element
+		if(items[i].innerHTML.toLowerCase().includes(inputTexto.toLowerCase())){
+			listaFiltrada.push(items[i]);	
+		}
+		listaNormal.push(items[i]);
+	}
+	
+	var listaNoBuscada = listaNormal.filter(
+	    function(elemento) {
+	      return this.indexOf(elemento) < 0;
+	    },
+	    listaFiltrada
+	)
+
+
+	if(listaNoBuscada.length > 0){
+		for (let k = 0; k < listaNoBuscada.length; ++k) {
+		  // do something with items[i], which is a <li> element
+		  listaNoBuscada[k].style.display = "none";
+		}
+	}
+	else{
+		MuestraTodosEventos(lista);
+	}
+}
